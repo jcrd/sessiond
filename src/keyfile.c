9a46909b39bdccc439f7bc8495c80991c09d3590
James Reed <jcrd@tuta.io>
2019-01-22 13:41:42 -0700

    Enhance logging of key file loading functions

--- src/keyfile.c
+++ src/keyfile.c
@@ -20,21 +20,33 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include <glib-2.0/glib.h>
 
+#define LOG_FORMAT "[%s] %s: %s"
+
+static gboolean
+handle_error(GError *err, const gchar *group, const gchar *key)
+{
+    gboolean ret = FALSE;
+    if (err->code == G_KEY_FILE_ERROR_KEY_NOT_FOUND ||
+        err->code == G_KEY_FILE_ERROR_GROUP_NOT_FOUND) {
+        ret = TRUE;
+        g_debug(LOG_FORMAT, group, key, err->message);
+    } else {
+        g_warning(LOG_FORMAT, group, key, err->message);
+    }
+
+    g_error_free(err);
+
+    return ret;
+}
+
+
 gboolean
 kf_load_bool(GKeyFile *kf, const gchar *g, const gchar *k, gboolean *opt)
 {
     GError *err = NULL;
     gboolean boolean = g_key_file_get_boolean(kf, g, k, &err);
-    if (err) {
-        gboolean ret = FALSE;
-        if (err->code == G_KEY_FILE_ERROR_KEY_NOT_FOUND ||
-            err->code == G_KEY_FILE_ERROR_GROUP_NOT_FOUND)
-            ret = TRUE;
-        else
-            g_warning("%s", err->message);
-        g_error_free(err);
-        return ret;
-    }
+    if (err)
+        return handle_error(err, g, k);
 
     *opt = boolean;
 
@@ -46,16 +58,8 @@ kf_load_str(GKeyFile *kf, const gchar *g, const gchar *k, gchar **opt)
 {
     GError *err = NULL;
     gchar *str = g_key_file_get_string(kf, g, k, &err);
-    if (err) {
-        gboolean ret = FALSE;
-        if (err->code == G_KEY_FILE_ERROR_KEY_NOT_FOUND ||
-            err->code == G_KEY_FILE_ERROR_GROUP_NOT_FOUND)
-            ret = TRUE;
-        else
-            g_warning("%s", err->message);
-        g_error_free(err);
-        return ret;
-    }
+    if (err)
+        return handle_error(err, g, k);
 
     *opt = str;
 
@@ -67,16 +71,8 @@ kf_load_int(GKeyFile *kf, const gchar *g, const gchar *k, gint *opt)
 {
     GError *err = NULL;
     gint i = g_key_file_get_integer(kf, g, k, &err);
-    if (err) {
-        gboolean ret = FALSE;
-        if (err->code == G_KEY_FILE_ERROR_KEY_NOT_FOUND ||
-            err->code == G_KEY_FILE_ERROR_GROUP_NOT_FOUND)
-            ret = TRUE;
-        else
-            g_warning("%s", err->message);
-        g_error_free(err);
-        return ret;
-    }
+    if (err)
+        return handle_error(err, g, k);
 
     *opt = i;
 
@@ -109,7 +105,7 @@ kf_load_exec(GKeyFile *kf, const gchar *g, const gchar *k, gchar ***opt)
     g_shell_parse_argv(str, NULL, &argv, &err);
 
     if (err) {
-        g_warning("%s", err->message);
+        g_warning(LOG_FORMAT, g, k, err->message);
         g_error_free(err);
         g_free(str);
         return FALSE;
@@ -128,16 +124,8 @@ kf_load_input_mask(GKeyFile *kf, const gchar *g, const gchar *k, guint *opt)
     gsize len;
     gchar **input = g_key_file_get_string_list(kf, g, k, &len, &err);
 
-    if (err) {
-        gboolean ret = FALSE;
-        if (err->code == G_KEY_FILE_ERROR_KEY_NOT_FOUND ||
-            err->code == G_KEY_FILE_ERROR_GROUP_NOT_FOUND)
-            ret = TRUE;
-        else
-            g_warning("%s", err->message);
-        g_error_free(err);
-        return ret;
-    }
+    if (err)
+        return handle_error(err, g, k);
 
     guint mask = 0;
 
