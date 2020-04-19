c5eae7b409c3b0521295cdc9e659db4ed55f226c
James Reed <jcrd@tuta.io>
2020-04-19 11:28:35 -0600

    Squashed 'src/toml/' changes from c32a6e9..cdbb9de
    cdbb9de Fixed issue #33
    eaa5d04 fix memory leak
    4078d23 restructure and clean up toml_rtos
    416be8d re-indent
    10787cb re-indent
    532239d Merge pull request #30 from theakman2/issue_29_strndup_memory_leak_fix
    7efd69c Fix for #29: STRNDUP memory leak fix
    b92d6a2 remove use of dummy errbuf due to benign gcc warnings
    f31bcd0 prettier prints
    f782b12 test output from jq needs to be sorted
    2453664 updated test2/run.sh
    987f4ef refine run.sh
    685ca97 refine run.sh
    896561d update test2
    9ed009b simplify example in README.md
    a1a8911 Updated Readme.md
    
    git-subtree-dir: src/toml
    git-subtree-split: cdbb9decfb315e9a51041fc91786f93d9baf6502

--- test2/run.sh
+++ test2/run.sh
@@ -1,16 +1,31 @@
+if ! (which jq >& /dev/null); then 
+    echo "ERROR: please install the 'jq' utility"
+    exit 1
+fi
+
 #
 #  POSITIVE tests
 #
 for i in toml-spec-tests/values/*.toml; do
-    echo -n $i ' '
-    ../toml_json $i >& $i.json.out
-    rc=$?
-    [ -f $i.json ] && diff=$(diff $i.json $i.json.out) || diff=''
-    if [ "$rc" != "0" ] || [ "$diff" != "" ]; then
-	echo '[FAILED]'
-    else
-	echo '[OK]'
+    fname="$i"
+    ext="${fname##*.}"
+    fname="${fname%.*}"
+    echo -n $fname ' '
+    res='[OK]'
+    if (../toml_json $fname.toml >& $fname.json.out); then 
+        jq -S . $fname.json.out > t.json
+	mv t.json $fname.json.out
+	if [ -f $fname.json ]; then
+	    if ! (diff $fname.json $fname.json.out >& /dev/null); then 
+	        res='[FAILED]'
+	    else
+		rm -f $fname.json.out
+	    fi
+	else
+	    res='[??]'
+	fi
     fi
+    echo ... $res
 done
 
 
@@ -19,13 +34,9 @@ done
 #
 for i in toml-spec-tests/errors/*.toml; do 
     echo -n $i ' '
-    ../toml_json $i >& $i.json.out
-    rc=$?
-    
-    if [ "$rc" != "0" ]; then
-	echo '[OK]'
-    else
-	echo '[FAILED]'
+    res='[OK]'
+    if (../toml_json $i >& $i.json.out); then 
+	res='[FAILED]'
     fi
-
+    echo ... $res
 done
