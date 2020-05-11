ef00fc49ce9172f8f3184191eda332f651a0125a
James Reed <jcrd@tuta.io>
2020-05-11 14:04:11 -0600

    Update copyright year to 2020

--- src/dbus-logind.c
+++ src/dbus-logind.c
@@ -1,6 +1,6 @@
 /*
 sessiond - standalone X session manager
-Copyright (C) 2018 James Reed
+Copyright (C) 2018-2020 James Reed
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /*
 sessiond - standalone X session manager
-Copyright (C) 2019 James Reed
+Copyright (C) 2019-2020 James Reed
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
